de9080e0b30c0a511efc7a5712a403d5bc46797a
Harpreet <preity.055@gmail.com>
2016-09-20 19:56:05 +0530

    Changes in Ex1 and Ex2

--- example1/dev/component-render.js
+++ example1/dev/component-render.js
@@ -2,5 +2,7 @@ var React = require('react'),
 	ReactDOM = require('react-dom'),
 	WelcomeComponent = require('./welcome-component');
 
-ReactDOM.render(<WelcomeComponent message="Developers" />,
-	document.getElementById("component-container"));
+ReactDOM.render(
+	<WelcomeComponent message="Developers" />,
+	document.getElementById("component-container")
+);
--- example2/dev/welcome-component.js
+++ example2/dev/welcome-component.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class WelcomeComponent extends React.Component {
 	render() {
-		return <h1>Welcome  {this.props.message}!</h1>;
+		return <h1>Hello {this.props.message}</h1>;
 	}
 }
 
